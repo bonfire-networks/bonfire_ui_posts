458a9c80d56f618235ac7354176f5ff45588f70c
Mayel de Borniol <git@mayel.space>
2024-02-04 20:07:23 +0000

    selected_tab

--- lib/components/profile_posts/profile_posts_live.ex
+++ lib/components/profile_posts/profile_posts_live.ex
@@ -3,7 +3,7 @@ defmodule Bonfire.UI.Posts.ProfilePostsLive do
 
   prop page_title, :string, required: true
   prop page, :string, required: true
-  prop selected_tab, :string, default: "posts"
+  prop selected_tab, :any, default: "posts"
   prop smart_input, :boolean, required: true
   prop smart_input_opts, :map, default: %{}
   prop search_placeholder, :string
