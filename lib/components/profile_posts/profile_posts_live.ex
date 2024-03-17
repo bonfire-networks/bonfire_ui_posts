852a5f289047191221e9157b4f62f31fa0e985e6
Mayel de Borniol <git@mayel.space>
2024-03-17 17:16:23 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/900

--- lib/components/profile_posts/profile_posts_live.ex
+++ lib/components/profile_posts/profile_posts_live.ex
@@ -1,18 +1,18 @@
 defmodule Bonfire.UI.Posts.ProfilePostsLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
+  prop user, :map
   prop page_title, :string, required: true
   prop page, :string, required: true
   prop selected_tab, :any, default: "posts"
   prop smart_input, :boolean, required: true
   prop smart_input_opts, :map, default: %{}
   prop search_placeholder, :string
-  prop feed_title, :string
-  prop user, :map
-  prop feed, :list
-  prop page_info, :any
+  prop feed_title, :string, default: nil
+  prop feed, :list, default: nil
+  prop page_info, :any, default: nil
   prop follows_me, :boolean, default: false
-  prop loading, :boolean, default: false
+  prop loading, :boolean, default: true
   prop feed_component_id, :any, default: nil
   prop hide_tabs, :boolean, default: false
 
